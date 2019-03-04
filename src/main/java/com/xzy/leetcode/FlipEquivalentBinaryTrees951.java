d601da76044eb4bab235b1228982dea662517da2
RussXia <xzy199400@gmail.com>
2019-03-04 10:40:23 +0800

    add solved

--- src/main/java/com/xzy/leetcode/FlipEquivalentBinaryTrees951.java
+++ src/main/java/com/xzy/leetcode/FlipEquivalentBinaryTrees951.java
@@ -9,57 +9,48 @@ import java.util.Objects;
 public class FlipEquivalentBinaryTrees951 {
 
     public static void main(String[] args) {
-//        TreeNode treeNode1 = new TreeNode(1);
-//        treeNode1.left = new TreeNode(2);
-//        treeNode1.left.left = new TreeNode(4);
-//        treeNode1.left.right = new TreeNode(5);
-//        treeNode1.left.right.left = new TreeNode(7);
-//        treeNode1.left.right.right = new TreeNode(8);
-//
-//        treeNode1.right = new TreeNode(3);
-//        treeNode1.right.left = new TreeNode(6);
-//
-//        TreeNode treeNode2 = new TreeNode(1);
-//        treeNode2.left = new TreeNode(3);
-//        treeNode2.left.right = new TreeNode(6);
-//
-//        treeNode2.right = new TreeNode(2);
-//        treeNode2.right.left = new TreeNode(4);
-//        treeNode2.right.right = new TreeNode(5);
-//        treeNode2.right.right.left = new TreeNode(8);
-//        treeNode2.right.right.right = new TreeNode(7);
-//
-//        System.out.println(flipEquiv(treeNode1, treeNode2));
+        TreeNode root1 = new TreeNode(0);
+        root1.left = new TreeNode(2);
+        root1.left.right = new TreeNode(6);
+        root1.right = new TreeNode(1);
+        root1.right.left = new TreeNode(3);
+        root1.right.left.left = new TreeNode(5);
+        root1.right.right = new TreeNode(4);
 
-        TreeNode treeNode3 = new TreeNode(0);
-        treeNode3.left = new TreeNode(3);
-        treeNode3.right = new TreeNode(1);
-        treeNode3.right.right = new TreeNode(2);
-
-        TreeNode treeNode4 = new TreeNode(0);
-        treeNode4.left = new TreeNode(3);
-        treeNode4.left.left = new TreeNode(2);
-        treeNode4.right = new TreeNode(1);
-        System.out.println(flipEquiv(treeNode3, treeNode4));
-
+        TreeNode root2 = new TreeNode(0);
+        root2.left = new TreeNode(4);
+        root2.right = new TreeNode(1);
+        root2.right.left = new TreeNode(2);
+        root2.right.left.left = new TreeNode(6);
+        root2.right.right = new TreeNode(3);
+        root2.right.right.left = new TreeNode(5);
+        System.out.println(flipEquiv(root1, root2));
     }
 
     public static boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) {
-            return true;
-        }
-        if (root1 == null || root2 == null) {
+        if (root1 == null || root2 == null)
+            return root1 == root2;
+        if (!Objects.equals(root1.val, root2.val))
             return false;
-        }
-        if (!Objects.equals(root1.val, root2.val)) {
-            return false;
-        }
-        if(flipEquiv(root1.left, root2.right)
-                || flipEquiv(root1.right, root2.left)
-                || flipEquiv(root1.left, root2.left)
-                || flipEquiv(root1.right, root2.right)) {
-            return true;
-        }
-        return false;
+        return (flipEquiv(root1.left, root2.right)
+                && flipEquiv(root1.right, root2.left))
+                || (
+                flipEquiv(root1.left, root2.left)
+                        && flipEquiv(root1.right, root2.right)
+        );
+    }
+
+    private static boolean checkNodeNum(TreeNode root1, TreeNode root2) {
+        int leafNode1 = 0;
+        if (root1.left != null)
+            leafNode1++;
+        if (root1.right != null)
+            leafNode1++;
+        int leafNode2 = 0;
+        if (root2.left != null)
+            leafNode2++;
+        if (root2.right != null)
+            leafNode2++;
+        return leafNode1 == leafNode2;
     }
 }
